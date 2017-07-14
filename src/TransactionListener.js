2bd3ebc0bc4c0866e5242e48fedcdbe523375e6d
David Klie <mangok543@gmail.com>
2017-07-13 23:20:20 -0400

    Added fnctions to stopWatching and provide contracts address, some minor improvements

--- src/TransactionListener.js
+++ src/TransactionListener.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase");
 var Web3 = require("web3");
-const fs = require("fs");
+
 
 function TransactionListener(web3Node,firebaseDatabase) {
     var err;
@@ -24,7 +24,8 @@ function TransactionListener(web3Node,firebaseDatabase) {
     this.eventIndex = {};
 }
 
-TransactionListener.prototype.loadContract = function(contractObject){
+TransactionListener.prototype.loadContract = function(contractObject,address){
+    const addrRegex = /^0x[0-9A-F]{40}$/;
     var err;
     var contract;
     if(!contractObject){
@@ -34,8 +35,34 @@ TransactionListener.prototype.loadContract = function(contractObject){
     }
     if(contractObject.abi){
         contract = web3.eth.contract(contractObject.abi);
+    };
+    if(address){
+        if(addrRegex.test(address)){
+            this.contract = contract.at(address);
+        } else {
+            err = new Error("Address is not a string or is not a valid hex");
+            err.message = "AddressNotValidError";
+            throw err;
+        }
+
     }
-    this.contract = contract;
+}
+
+TransactionListener.prototype.setAddressContract = function(address){
+    var err;
+    const addrRegex = /^0x[0-9A-F]{40}$/;
+    if(!this.contract){
+        eerr = new Error("Cotract attribute undefined");
+        err.message = "UndefinedContractError";
+        throw err;
+    }
+    if(addrRegex.test(address)){
+        this.contract = contract.at(address);
+    } else {
+        err = new Error("Address is not a string or is not a valid hex");
+        err.message = "AddressNotValidError";
+        throw err;
+    }
 }
 
 TransactionListener.prototype.listenToEvent = function(eventName,customName) {
@@ -67,4 +94,24 @@ TransactionListener.prototype.listenToEvent = function(eventName,customName) {
     }
     this.eventIndex[(customName) ? customName : eventName] = eventObject;
 }
+
+TransactionListener.prototype.stopListening = function(eventName) {
+    if(eventName && typeof eventName !== 'string'){
+        err = new Error("Custom name must be string and not empty");
+        err.message = "EventNameNotString";
+        throw err;
+    }
+    var returnValue;
+    try{
+        this.eventIndex[eventName].stopWatching();
+        returnValue = true;
+    } catch(err) {
+        returnValue = false;
+    } 
+    return returnValue;
+}
+
+module.exports = TransactionListener;
+
+
 
